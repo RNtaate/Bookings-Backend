3dc004c247c47c9dad0127d470c47f5d07e2a1b8
RNtaate <royhyde14@gmail.com>
2021-06-21 14:30:03 +0300

    Add more routes for sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :sessions, only: [:create]
   resources :registrations, only: [:create]
+  delete :logout, to: 'sessions#logout'
+  get :logged_in, to: 'sessions#logged_in'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

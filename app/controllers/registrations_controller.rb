667784380d839ff906d3ac465b9daa86ce0a094b
RNtaate <royhyde14@gmail.com>
2021-06-22 02:33:24 +0300

    Update create method in RegistrationsController.rb file to pass registration errors to front end

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,9 @@
 class RegistrationsController < ApplicationController
 
   def create
-    user = User.create!(username: params["user"]["username"], password: params["user"]["password"], password_confirmation: params["user"]["password_confirmation"])
+    user = User.new(username: params["user"]["username"], password: params["user"]["password"], password_confirmation: params["user"]["password_confirmation"])
 
-    if user
+    if user.save
       session[:user_id] = user.id
       render json: {
         status: :created,
@@ -12,7 +12,8 @@ class RegistrationsController < ApplicationController
       }
     else
       render json: {
-        status: 500
+        status: 500,
+        errors: user.errors
       }
     end
   end

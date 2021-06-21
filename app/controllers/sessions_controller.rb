c876fc6880ac7ce7eaa291a936746a72c6b18243
RNtaate <royhyde14@gmail.com>
2021-06-21 14:29:47 +0300

    Create logged_in, logout functions in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,7 @@
 class SessionsController < ApplicationController
   
+  include CurrentUserConcern
+
   def create
     user = User.find_by(username: params["user"]["username"]).try(:authenticate, params["user"]["password"]);
 
@@ -15,6 +17,27 @@ class SessionsController < ApplicationController
         status: 401
       }
     end
+  end
+
+  def logged_in
+    if @current_user
+      render json: {
+        logged_in: true,
+        user: @current_user
+      }
+    else
+      render json: {
+        logged_in: false
+      }
+    end
+  end
+
+  def logout
+    reset_session
+    render json: {
+      status: 200,
+      logged_out: true
+    }
   end
 
 end

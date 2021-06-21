3e5df8b14c618bee926b9307c595d355df1049fa
RNtaate <royhyde14@gmail.com>
2021-06-21 11:49:56 +0300

    Create User model, run database migration

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :username, length: {minimum: 3}
+end

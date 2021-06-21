236b2b75c8e7c9b2439d291f5a58970c5bbff3ad
RNtaate <royhyde14@gmail.com>
2021-06-21 14:44:53 +0300

    Update session_store.rb file

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_bookings_app", domain: "norp-bookings-api.herokuapp.com"
+if Rails.env == 'production'
+  Rails.application.config.session_store :cookie_store, key: "_bookings_app", domain: "norp-bookings-api.herokuapp.com"
+else
+  Rails.application.config.session_store :cookie_store, key: "_bookings_app"
+end

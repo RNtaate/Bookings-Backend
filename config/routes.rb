3c1c6fcf2781ee7a1c258362ee716998684233f1
RNtaate <royhyde14@gmail.com>
2021-06-24 15:58:47 +0300

    Adding routing for massage model controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :registrations, only: [:create]
   delete :logout, to: 'sessions#logout'
   get :logged_in, to: 'sessions#logged_in'
+
+  resources :massages, only: [:index, :create, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

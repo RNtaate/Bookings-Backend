dce4e5ef773ad4da59e9c57c9f3d226da9911a36
RNtaate <royhyde14@gmail.com>
2021-06-26 00:44:43 +0300

    Add routes for appointments controller, add show route for massage controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   delete :logout, to: 'sessions#logout'
   get :logged_in, to: 'sessions#logged_in'
 
-  resources :massages, only: [:index, :create, :update, :destroy]
+  resources :massages, only: [:index, :show, :create, :update, :destroy]
+
+  resources :appointments, only: [:index, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

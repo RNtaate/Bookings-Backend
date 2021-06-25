67135e33feec1a14fe5cd9b611cae7a0b213e45b
RNtaate <royhyde14@gmail.com>
2021-06-26 00:43:56 +0300

    Complete association between user and appointment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :appointments, dependent: :destroy
+
   validates :username, presence: true
   validates :username, uniqueness: true
   validates :username, length: {minimum: 3}

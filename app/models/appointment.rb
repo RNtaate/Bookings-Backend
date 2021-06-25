4957d9f892ef139db4987483443a96df16f6cf87
RNtaate <royhyde14@gmail.com>
2021-06-26 00:43:23 +0300

    Add validations to appointments model

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,9 @@
 class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :massage
+
+  validates :customer_name, presence: true
+  validates :massage_type, presence: true
+  validates :date, presence: true
+  validates :city, presence: true
 end

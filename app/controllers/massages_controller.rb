eaf0ad3d8e8f7dc7cc72a04427880aa909e118c7
RNtaate <royhyde14@gmail.com>
2021-06-26 00:45:00 +0300

    Add show action controller

--- app/controllers/massages_controller.rb
+++ app/controllers/massages_controller.rb
@@ -5,6 +5,18 @@ class MassagesController < ApplicationController
     render json: @massages
   end
 
+  def show
+    @massage = Massage.find(params[:id]);
+
+    if @massage
+      render json: @massage
+    else
+      render json: {
+        status: 401
+      }
+    end
+  end
+
   def create
     @massage = Massage.create(massage_params)
     if @massage

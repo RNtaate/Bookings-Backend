2c49c5d2726cb9cf5f7d76343f9082669ae85316
RNtaate <royhyde14@gmail.com>
2021-06-27 16:34:47 +0300

    Add validations to massage model

--- app/models/massage.rb
+++ app/models/massage.rb
@@ -1,3 +1,20 @@
 class Massage < ApplicationRecord
   has_one_attached :massage_image
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :description, presence: true
+  validates :duration, presence: true
+  validates :price, presence: true
+
+  validate :acceptable_image
+
+  def acceptable_image
+    return unless massage_image.attached?
+
+    unless massage_image.byte_size <= 1.megabyte
+      errors.add(:massage_image, "is too big, Image must have maximum of 1mb")
+    end
+  end
+
 end

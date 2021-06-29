4fbf0b2074cd526a2d6859580e988d8832007060
RNtaate <royhyde14@gmail.com>
2021-06-29 10:58:22 +0300

    Add testing for the project models

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it {should validate_presence_of(:username)}
+
+  it {should validate_presence_of(:username)}
+
+  it {should validate_uniqueness_of(:username)}
+
+  it {should have_many(:appointments).dependent(:destroy)}
+end

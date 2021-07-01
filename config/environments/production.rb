71a7e175128309549905862c75f65d99593467b4
RNtaate <royhyde14@gmail.com>
2021-07-01 12:45:28 +0300

    Add default url options link

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_mailer.default_url_options = { :host => "serene-depths-82382.herokuapp.com" }
 end

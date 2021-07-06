7575b99fdc917268a672db020ca900c2676bfbf1
RNtaate <royhyde14@gmail.com>
2021-07-06 17:37:14 +0300

    Remove unwanted file and routing

--- app/controllers/welcome_controller.rb
+++
@@ -1,5 +0,0 @@
-class WelcomeController < ApplicationController
-  def index
-    render json: { status: 'Welcome to the Norp Massage Parlor Api' }
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,5 @@ Rails.application.routes.draw do
 
   resources :appointments, only: [:index, :create, :destroy]
 
-  root "welcome#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
